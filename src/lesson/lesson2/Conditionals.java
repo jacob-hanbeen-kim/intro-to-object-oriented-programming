08a2fc08c92188636cf7feecfb6b07f5c41d9bdf
Jacob Kim <jacobkim@jacobs-mbp.lan>
2021-12-21 18:58:07 -0500

    lesson 2 note

--- src/lesson/lesson2/Conditionals.java
+++ src/lesson/lesson2/Conditionals.java
@@ -6,28 +6,46 @@ public class Conditionals {
         int a = 5;
         int b = 10;
 
+        /** If, Else, Else If **/
+
         /** Less than: a < b **/
 
+        if (a < b) {
+            System.out.println("A is less than B!");
+        }
 
         /** Less than or equal to: a <= b **/
-
+        else if (a <= b) {
+            System.out.println("A is less than or equal to B!");
+        }
 
         /** Greater than: a > b **/
+
+        else if (a > b) {
+            System.out.println("A is greater than B!");
+        }
 
 
         /** Greater than or equal to: a >= b **/
 
+        else if (a >= b) {
+            System.out.println("A is greater than or equal to B!");
+        }
 
         /** Equal to a == b **/
-
-
         /** Not Equal to: a != b **/
-
-
-        /** If, Else, Else If **/
+        else {
+            System.out.println("A is equal to B!");
+            System.out.println("or A does not equal to B!");
+        }
 
 
         /** Ternary Operation **/
+
+        // variable = (condition) ? expressionTrue : expressionFalse
+        int age = 21;
+        String status = (age > 18) ? "Adult" : "Child";
+        System.out.println(status);
 
     }
 }
--- src/lesson/lesson2/ScannerDemo.java
+++ src/lesson/lesson2/ScannerDemo.java
@@ -11,13 +11,37 @@ public class ScannerDemo {
 
         /** System.in **/
 
+        System.out.println("Hello World!");
+
+        Scanner input = new Scanner(System.in);
+
         // nextXYZ() -- nextInt(), nextShort(), nextFloat()
 
+        System.out.print("What is your age? ");
+        int age = input.nextInt();
+
+        System.out.print("How much do you weight? ");
+        float weight = input.nextFloat();
+
+        System.out.print("How tall are you? ");
+        double height = input.nextDouble();
+
         // nextLine()
-
         // next()
-
         // charAt(0)
+
+        System.out.print("What's your first name? ");
+        String name = input.next();
+
+        System.out.print("What's your last name? ");
+        String lastName = input.next();
+
+
+        System.out.println("Hi " + name + " " + lastName.charAt(0) + "!");
+        System.out.println("Your age is " + age);
+        System.out.println("Your weight is " + weight);
+        System.out.println("Your height is " + height);
+
 
         /** Input File **/
 
